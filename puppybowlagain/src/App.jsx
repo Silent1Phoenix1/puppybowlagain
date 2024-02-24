40020d1070a6cae4534050dd926f622edcb1b2f0
Nathan <silentphoenix11@SilentPhoenix11.localdomain>
2024-02-23 19:18:46 -0500

    idk

--- puppybowlagain/src/App.jsx
+++ puppybowlagain/src/App.jsx
@@ -1,10 +1,21 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import { createPlayer } from './api'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  
+  useEffect(() => {
+    createPlayer({
+      name: "Rufus",
+      breed: "Irish Setter",
+    }).then((newPlayer) => {
+      console.log(newPlayer);
+    });
+  }, []); 
+
 
   return (
     <>
